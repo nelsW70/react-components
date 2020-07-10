82cf423032569d34c21e328a5677df59e94d684e
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-10 13:52:04 -0400

    add: comment and image props passed down to child

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        image={faker.image.avatar()}
+        comment="nice blog post!"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        image={faker.image.avatar()}
+        comment="sweet!"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        image={faker.image.avatar()}
+        comment="learn to read!"
+      />
     </div>
   );
 };
