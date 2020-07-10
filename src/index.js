6bcb01afec4f2f5369d3dcb8581ef198e7ad5369
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-10 16:06:53 -0400

    add: showing custom children in approval card

--- src/index.js
+++ src/index.js
@@ -7,25 +7,32 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        content="nice blog post!"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        avatar={faker.image.avatar()}
-        content="sweet!"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        avatar={faker.image.avatar()}
-        content="learn to read!"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          content="nice blog post!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          avatar={faker.image.avatar()}
+          content="sweet!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          avatar={faker.image.avatar()}
+          content="learn to read!"
+        />
+      </ApprovalCard>
     </div>
   );
 };
